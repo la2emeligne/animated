e5fddc7dad67c92857a74656f55ffd3b0effe999
Emoltroll <tristanlambert31@gmail.com>
2019-06-28 09:02:09 +0200

    1er commit

---
+++ POO_TDpoint/src/InsertPoint.java
@@ -0,0 +1,18 @@
+
+public class InsertPoint {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Point p1 = new Point(2,3);
+		Point p2 = new Point(1,4);
+		Point p3 = new Point(0,2);
+		
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+		
+		// 2nd méthode pour afficher la distance
+		//System.out.println("Distance = " + p1.distance());
+	}
+
+}
---
+++ POO_TDpoint/src/Point.java
@@ -0,0 +1,49 @@
+
+public class Point {
+	
+	// abscisse et ordonnée
+	private double X;
+	private double Y;
+	
+	public Point(double x, double y){
+		X = x;
+		Y = y;
+	}
+
+	/*public int getId() {
+		return Id;
+	}
+
+	public void setId(int id) {
+		Id = id;
+	}*/
+
+	public double getX() {
+		return X;
+	}
+
+	public void setX(double x) {
+		X = x;
+	}
+
+	public double getY() {
+		return Y;
+	}
+
+	public void setY(double y) {
+		Y = y;
+	}
+	
+	// Calcul de la distance
+	public double distance() {
+		return Math.sqrt(Math.pow(this.X, 2) + Math.pow(this.Y, 2)); // retourne le résultat lors d'un appel
+	}
+
+	// appel de la fonction distance dans le string
+	@Override
+	public String toString() {
+		return "Point : (" + X + "," + Y + ") " + "\nDistance = " + this.distance();
+	}
+	
+	
+}
